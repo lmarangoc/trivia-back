cd30263ca74ddfd872a3b2fb03682f26dbcdf8cf
Laura Marcela Arango Correa <mlarango@uninorte.edu.co>
2024-06-28 21:18:15 -0500

    added mongoose

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from "express"
 import mongoose from "mongoose"
+import {Schema, model} from "mongoose"
+
 
 const app = express()
 const port = process.env.PORT 
@@ -18,6 +20,47 @@ try {
     console.log("Database connected");
 } catch (error) {
     console.log("Error database", error);
+}
+
+const user_schema = Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    nickname: {
+        type: String,
+        require: true
+    },
+    cel: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const user = model("user", user_schema)
+
+const first_user = new user({
+    name: "John",
+    nickname: "jjatt",
+    cel: "12345",
+    password: "123"
+})
+
+try {
+    await first_user.user.save()
+} catch(error) {
+    console.log(error);
 }
 
 app.listen(port, () => console.log(`Server on port: ${port}`))
